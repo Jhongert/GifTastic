b5879ecb2ebb9f05ab1d553cdd59b2ac0479c37d
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-06-10 01:07:10 -0400

    update comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,52 @@
 //array to hold the buttons
 var sports = ["baseball", "boxing", "diving", "football", "golf", "gymnastics", "hockey",
-			"nba", "parkour", "rowing", "formula one", "horse racing",
+			"basketball", "parkour", "rowing", "formula one", "horse racing",
 			"martial arts", "rock climbing", "roller skating", "skateboarding"
 	];
 
 //query url
 var url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=12&q=";
+
+//array to prefetch the gif
 var gifs = [];
 
-//function to create buttons
-function createButtons(){
+//this function create a button
+//this function is called from createButtons function and from add button click event
+function createBtn(value){
 	var buttons = $("#buttons");
-	for(var i = 0; i < sports.length; i++){
-		buttons.append("<button class='btn' value='"+ sports[i] + "'>" + sports[i] + "</button>");
+	buttons.append("<button class='btn' value='"+ value + "'>" + value + "</button>");
+}
+
+//this function loop the array sports and call createBtn to create each button
+function createButtons(arr){
+	for(var i = 0; i < arr.length; i++){
+		createBtn(arr[i]);
 	}
 }
 
-$(document).ready(function(){
-	createButtons();
+//function to show messages when we add a new sport, when the sport already exist
+//or when we try to add a sport with a empty input
+function showMsg(msg){
+	$("#msg").text(msg).slideDown(); //show the message
+	setTimeout("$('#msg').slideUp()", 3000); //hide the div after 3 sec
+}
 
+$(document).ready(function(){
+	createButtons(sports); //create buttons
+
+	//click event for buttons
 	$("#buttons").on("click", ".btn", function(){
 		
 		var term = $(this).val(); //value of the clicked button
-		var queryURL = url + term; //to create the query 
+		var queryURL = url + term; //Add the seach term to the query 
 		
+		//create a h1 with the selected sport
 		var header = $("<h1>");
 		header.text(term.toUpperCase());
 
-		$("#images").empty().append(header);
+		$("#images").empty().append(header); //remove images from container and add the h1
 
+		//ajax request
 		$.ajax({
       		url: queryURL,
       		method: "GET"
@@ -36,37 +54,45 @@ $(document).ready(function(){
     			
     			var result = response.data;
 
+    			//loop through the response object
     			for(var i = 0; i < result.length; i++){
-    			 	var image = result[i].images.fixed_width_still.url; //URL to the image
-    			 	var gif = result[i].images.fixed_width.url; //URL to the gif
+
+    			 	var image = result[i].images.fixed_width_still.url; //static image url
+    			 	var gif = result[i].images.fixed_width.url; //gif url
 
 
-					var li = $("<li class='itemList'>");	//to create a div
+					var li = $("<li class='listItem'>"); //Create a li element and add class listItem
 
-					//To create a p and add the rating
+					//Create a p element, add text with the rating and append it to the li element
 					var p = $("<p>");
     				p.text("Rating: " + result[i].rating);
     				li.append(p);
 
+    				//Create a div with class imgHolder, container for the image
     				var div = $("<div class='imgHolder'>");
 
-    				//To create a image, add class and attr
-    				var img = $("<img class='gif'>");
+    				//Create a image, add class, attr and append it to the div element
+    				var img = $("<img class='image'>");
     				img.attr({src: image, "data-gif": gif, "data-play": false, "data-still": image});
     				div.append(img);
 
+    				//append the div to li emenent
     				li.append(div);
+
+    				//append li element to his parent ul with id = images
     				$("#images").append(li);
 
     				
     				//Prefetch gif images
-    			 	gifs[i] = new Image();
-    			 	gifs[i].src = gif;
+    				//This allows the gif to play faster when we click a image
+    			 	gifs[i] = new Image(); //Create a new html image
+    			 	gifs[i].src = gif; //assign the gif url to its src attribute
       			}	
     		});
 	});
 
-	$("#images").on("click", ".gif", function(){
+	//click event for images
+	$("#images").on("click", ".image", function(){
 		var thisImg = $(this); //image that was clicked
 
 		//get attribute data-play from the image
@@ -87,33 +113,41 @@ $(document).ready(function(){
 		}
 	});
 
+	//click event for add button
 	$("#add").click(function(){
-		var text = $("input[type=text]").val(); //get text from text input
+		var text = $("input[type=text]").val(); //get the text from text input
 		text = text.trim().toLowerCase(); //remove spaces and convert it to lowercase
-		if(text == ""){
-			$("#msg").text("You have to enter a sport.").slideDown();
-			setTimeout("$('#msg').slideUp()",3000);
+		var msg = "";
+
+		if(text == ""){ //check if the imput is empty, if it is, we will display a message
+			msg = "You have to enter a sport.";	
 			
-		} else{
-			//if the input is not already in the array
+		} else { //if the input is not empty
+
+			//check if the input exist in the array
 			var index = sports.indexOf(text);
-			if( index == -1){ 
-				var btn = $("<button>"); 		//create the button
-				btn.addClass("btn"); 			//add clase btn
-				btn.val(text).text(text); 		//add text
-				$("#buttons").append(btn); 		//add the button to the list
+
+			if( index == -1){ 					//the input is not in the array
+				createBtn(text);				//create a button
+
 				$("input[type=text]").val(""); 	//empty the input
 				sports.push(text); 				//add new button to array
-				$("#msg").text("The sport has been added.").slideDown();
-				setTimeout("$('#msg').slideUp()",3000);
-			} else{
-				$("#msg").text("This sport already exist.").slideDown();
+
+				msg = "The sport has been added.";
+
+			} else { //if the input exist in the array(if there is a button with that value)
+
+				msg = "This sport already exist."
+
+				//add a class to highlight the button with that value 
 				$("#buttons .btn:eq(" + index + ")").addClass("glowing");
+
+				//after 3 sec remove the class from the button
 				setTimeout(function(){
-					$('#msg').slideUp();
 					$("#buttons .btn:eq(" + index + ")").removeClass("glowing");
 				}, 3000);
 			}
 		}
+		showMsg(msg); //show the message
 	});
 });
